cde5bed3b6b89762b27dd77a03fa27a1847b1893
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-05-23 05:52:22 +0530

    login api added

--- controllers/authController.js
+++ controllers/authController.js
@@ -38,4 +38,29 @@ const signup = async(req,res)=>{
     }
 }
 
-module.exports= { signup }
+const login = async (req,res)=>{
+    try{
+        const {email, password} = req.body;
+
+        const user = await User.findOne({email})
+        if (!user) {
+            return res.status(401).json({message: "invalid credentials"})
+        }
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch){
+            return res.status(401).json({message :"invalid credentials"})
+        }
+
+        const token = jwt.sign(
+            { userId: user._id},
+            process.env.JWT_SECRET,
+            {expiresIn: '1h'}
+        )
+        res.status(200).json({message: "login succesfull", token})
+    }
+    catch (error){
+        res.status(500).json({message: 'login failed', error: error.message})
+    }
+}
+
+module.exports= { signup, login }
--- server.js
+++ server.js
@@ -38,5 +38,6 @@ app.get('/', (req,res)=>{
 })
 
 app.use('/api/auth', authRoutes)
+app.use('/api/auth', authRoutes)
 
 
