59d8c5ffd82f1a7e924e93fa1d1d0c10072e7313
Bharath Kumar Reddy <bharathkumar787@icloud.com>
2025-05-28 04:32:59 +0530

    frontend pages

---
+++ client/src/App.jsx
@@ -0,0 +1,31 @@
+
+// import './App.css'
+import './index.css'
+
+
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Signup from './components/Signup'
+import Login from './components/Login'
+import Caps from './components/Caps'
+
+
+
+function App() {
+  
+
+  return (
+    <BrowserRouter>
+      <div className='min-h-screen bg-black'>
+        <Routes>
+          <Route path='/signup' element={<Signup />} />
+          <Route path='/login' element={<Login />} />
+          <Route path='/capsules' element={<Caps />} />
+          <Route path='/' element={<Login />} />
+        </Routes>
+        
+      </div>
+    </BrowserRouter>
+  )
+}
+
+export default App
---
+++ client/src/components/Caps.jsx
@@ -0,0 +1,9 @@
+function Caps() {
+  return (
+    <div className="p-4">
+      <h2 className="text-2xl font-bold text-gray-200">Capsules Page</h2>
+    </div>
+  );
+}
+
+export default Caps;
